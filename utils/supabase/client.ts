76d8e9268031b466fa7d5550ef25259512c162c9
turtle1673 <66026189@up.ac.th>
2025-02-06 22:48:45 +0700

    fig some issue

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://ewpxbkowozipvpeddfux.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3cHhia293b3ppcHZwZWRkZnV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NDkxMTAsImV4cCI6MjA1MzIyNTExMH0.OgkYTT_DM2b57iP5mpErrdQiUqR_0fJBMMusU7tRWOg'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 export const supabase = createClient(supabaseUrl, supabaseKey)
