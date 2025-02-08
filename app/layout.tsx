3cdeeb4e66fc23015f149a2e5770fd396fcecf2a
nickykung123 <Email>
2025-02-09 04:11:02 +0700

    New html

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,15 +16,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className='' style={{ 
-  backgroundImage: "url('https://i.pinimg.com/originals/0e/0d/72/0e0d72395ecbc6682ea8e7276bc6fe06.gif')", 
-  backgroundSize: 'contain',  // Ensures the image fits without distortion
-  backgroundPosition: 'center', 
-  backgroundAttachment: 'fixed',  // Keeps the image fixed when scrolling
-  width: '100vw', 
-  height: '100vh', 
-  margin: '0', 
-  overflow: 'hidden' 
-}}>
+                backgroundImage: "url('https://i.pinimg.com/originals/0e/0d/72/0e0d72395ecbc6682ea8e7276bc6fe06.gif')", 
+                backgroundSize: 'contain',  // Ensures the image fits without distortion
+                backgroundPosition: 'center', 
+                backgroundAttachment: 'fixed',  // Keeps the image fixed when scrolling
+                width: '100vw', 
+                height: '100vh', 
+                margin: '0', 
+                overflow: 'hidden' 
+              }}>
           <Navbar/>
           <div className='justify-self-center'>
           {children}
