1569eeacb4def555aab63b04e5a94a88f10b2a40
nickykung123 <Email>
2025-02-10 05:22:38 +0700

    Login update

---
+++ app/auth/confirm/route.js
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type')
+  const next = searchParams.get('next') ?? '/'
+
+  const redirectTo = request.nextUrl.clone()
+  redirectTo.pathname = next
+  redirectTo.searchParams.delete('token_hash')
+  redirectTo.searchParams.delete('type')
+
+  if (token_hash && type) {
+    const supabase = await createClient()
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+    if (!error) {
+      redirectTo.searchParams.delete('next')
+      // redirect user to specified redirect URL or root of app
+      return NextResponse.redirect(redirect)
+    }
+  }
+
+  // redirect the user to an error page with some instructions
+  redirectTo.pathname = '/error'
+  return NextResponse.redirect(redirectTo)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,11 @@
-"use client"
-
-// import { useUser } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { logout } from "./logout/actions";
+import DemoClientComponent from "./components/DemoClientComponent";
 
 export default function Home() {
-
-  const router = useRouter()
-  
-
-  // const {user,isSignedIn} = useUser();
-  
-  // useEffect(() => {
-  //   if (isSignedIn) {
-  //     router.push("/sugarcalculator");
-  //   }
-  // }, [isSignedIn, router]);
-
-  const [bloodvalue,setBloodvalue] = useState('')
-  const [result,setResult] = useState('')
-  const [errorform,setErrorform] = useState('')
-
-  function calSugar(value:number){
-    let sugarResult = ''
-    if(value<70 || value == 0){
-      sugarResult = 'คุณมีระดับน้ำตาลน้อยกว่าปกติ'
-  }else if(value>=70 && value<=100){
-      sugarResult = 'ปกติ'
-  }else if(value>100 && value<=125){
-      sugarResult = 'มีความเสี่ยง'
-  }else{
-    sugarResult = 'มีความเสี่ยงสูง'
-  }
-
-  return sugarResult
-  }
-
-
-  const handlerSubmit = (e:any) => {
-    e.preventDefault()
-
-    if(!bloodvalue){
-      setErrorform('จำเป็นต้องใส่ข้อมูลตัวเลขระดับน้ำตาลของคุณ')
-      setResult('')
-      return
-    }
-    const numValue = parseFloat(bloodvalue)
-    if(!numValue){
-      setErrorform('ข้อมูลต้องเป็นตัวเลขจำนวนเต็มบวก')
-      setResult('')
-      return
-    }
-
-    setResult(calSugar(numValue))
-    setErrorform('')
-    setErrorform('')
-  }
-
   return (
-
-    <>
-
-    </>
+    
+    <main className="">
+    </main>
   );
 }
