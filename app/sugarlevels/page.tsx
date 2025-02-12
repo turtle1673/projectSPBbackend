1e6b0c0f9f890c6d35f82f9e5c40d1415289e369
nickykung123 <Email>
2025-02-13 04:39:05 +0700

    sugarlevel

--- app/sugarlevels/page.tsx
+++ app/sugarlevels/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import SugarlevelsForm from '../components/SugarlevelsForm'
-import SugarForm from '../components/SugarForm'
 
 export default async function sugar_levels() {
   return (
     <>
     {/* <SugarlevelsForm /> */}
-    <SugarForm />
+    <SugarlevelsForm />
     </>
   )
 }
